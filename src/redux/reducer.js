25ea409f5cee2669afde534ebdc25d244f49d4ff
Vũ Duy <ducduy23899@gmail.com>
2022-05-25 23:56:43 +0700

    update

---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+import { STAFFS, DEPARTMENTS } from '../shared/staffs';
+export const initialState = {
+    staffs: STAFFS,
+    department: DEPARTMENTS,
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+};
