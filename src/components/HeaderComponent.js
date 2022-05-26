4dda88059fec321db758b9f944447d8bb3ee5557
Vũ Duy <ducduy23899@gmail.com>
2022-05-26 22:01:37 +0700

    repo lab05

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -67,7 +67,7 @@ class Header extends Component {
                             </Collapse>
                         </div>
                     </Navbar>
-                    {/* <Jumbotron>
+                    <div className="Jumbotron">
                         <div className="container">
                             <div className="row row-header">
                                 <div className="col-12 col-sm-6">
@@ -76,7 +76,7 @@ class Header extends Component {
                                 </div>
                             </div>
                         </div>
-                    </Jumbotron> */}
+                    </div>
 
 
                 </React.Fragment>
