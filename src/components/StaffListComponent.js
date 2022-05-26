61ce60a711f162b0e3235d8bc9ffdac4104e2ce5
Vũ Duy <ducduy23899@gmail.com>
2022-05-26 08:51:19 +0700

    update package

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
+import {Button} from 'react-bootstrap';
 
 import DetailStaffComponent from "./DetailStaffComponent";
 import '../App.css';
