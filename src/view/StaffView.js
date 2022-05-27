0ae66d81437d54606167daf829222085957f6b4b
Vũ Duy <ducduy23899@gmail.com>
2022-05-27 11:27:21 +0700

    routes

--- src/view/StaffView.js
+++ src/view/StaffView.js
@@ -1,41 +1,45 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { STAFFS, DEPARTMENTS } from '../shared/staffs';
 import '../App.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import StaffList from '../components/StaffList';
 import StaffDetail from '../components/StaffDetail';
 import Department from '../components/Department';
 import Salary from '../components/Salary';
 
-function StaffView() {
-    const [staff] = useState({
-        staffs: STAFFS,
-        department: DEPARTMENTS,
+class StaffView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            staffs: STAFFS,
+            department: DEPARTMENTS,
 
-    });
-
-    const StaffWithId = ({ match }) => {
+        }
+    };
+    render() {
+        const StaffWithId = ({ match }) => {
+            return (
+                <StaffDetail staff={this.state.staffs.filter(item =>
+                    item.id === parseInt(match.params.staff_id, 10))[0]}></StaffDetail>
+            )
+        }
         return (
-            <StaffDetail staff={staff.staffs.filter(item =>
-                item.id === parseInt(match.params.staff_id, 10))[0]}></StaffDetail>
+            <div>
+                <Header />
+                <div>
+                    <Routes>
+                        <Route index path="/staff"
+                            element={<StaffList staffs={this.state.staffs}></StaffList>}></Route>
+                        <Route path="/staff/:staff_id" element={StaffWithId}></Route>
+                        <Route path="/department/" element={<Department dept={this.state.department} />}></Route>
+                        <Route path="/salary/" element={<Salary salarys={this.state.staffs}></Salary>}></Route>
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
         )
     }
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route
-                    exact path="/staff"
-                    component={() => <StaffList staffs={staff.staffs}></StaffList>}></Route>
-                <Route path="/staff/:staff_id" component={StaffWithId}></Route>
-                <Route path="/department/" component={() => <Department dept={staff.department} />}></Route>
-                <Route path="/salary/" component={() => <Salary salarys={staff.staffs}></Salary>}></Route>
-            </Switch>
-            <Footer />
-        </div>
-    )
 }
-
 export default StaffView;
