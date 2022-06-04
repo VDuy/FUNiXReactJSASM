ae7fde8cdc0096789f601697d2e96d8d217b35d1
Vũ Duy <ducduy23899@gmail.com>
2022-06-04 15:29:34 +0700

    build and deploy your React application.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -31,9 +31,6 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class MainComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount() {
     this.props.fetchDishes();
